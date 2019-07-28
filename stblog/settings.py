ed68ba18d819023e298bdd3a648f876ba83aca7d
HyunJung27 <rkacks1509@gmail.com>
2019-07-28 23:28:52 +0900

    deploy_st

--- stblog/settings.py
+++ stblog/settings.py
@@ -20,11 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'e9w-=$yb#5_92$a2-w@z-76mto_!r5^p)hc_865og$m#8y+f49'
+#SECRET_KEY = 'e9w-=$yb#5_92$a2-w@z-76mto_!r5^p)hc_865og$m#8y+f49'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'e9w-=$yb#5_92$a2-w@z-76mto_!r5^p)hc_865og$m#8y+f49')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+#DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 ALLOWED_HOSTS = []
 
 
