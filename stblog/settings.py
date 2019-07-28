984a3e9f6bc58d8b5bca5c951983dc38e015520b
HyunJung27 <rkacks1509@gmail.com>
2019-07-28 23:39:15 +0900

    deploy_3

--- stblog/settings.py
+++ stblog/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'e9w-=$yb#5_92$a2-w@z-76mto_!r5
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
